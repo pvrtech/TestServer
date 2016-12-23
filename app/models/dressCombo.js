75b6ea2b7fb387d9987f5fb3277f785bdd690f42
PVR <pvrtech2014@cdk.com>
2016-12-23 21:10:21 +0530

    Initial Commit

---
+++ app/models/dressCombo.js
@@ -0,0 +1,15 @@
+// load the things we need
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+// define the schema for our Combo Dress model
+var comboDressSchema = mongoose.Schema({
+    comboid		 : String,
+    type         : Array,
+    userid       : String,
+    dressid      : String,
+    events       : Array
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('ComboDress', comboDressSchema);
